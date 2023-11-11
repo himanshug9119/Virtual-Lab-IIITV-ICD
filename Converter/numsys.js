0f61d3b2084d2625933771702823b6d8acf0515f
Himanshu Gupta <guptasitapur489@gmail.com>
2023-11-11 10:53:41 +0530

    Updated 11/11/2023 final

--- Converter/numsys.js
+++ Converter/numsys.js
@@ -1,57 +1,74 @@
-document.addEventListener("contextmenu", function(e){
+
+document.addEventListener(
+  "contextmenu",
+  function (e) {
     e.preventDefault();
-}, false);
+  },
+  false
+);
 
 let d = document.getElementById("d"),
-b = document.getElementById("b"),
-o = document.getElementById("o"),
-h = document.getElementById("h")
-m = document.getElementById("m")
-l = document.getElementById("l")
+  b = document.getElementById("b"),
+  o = document.getElementById("o"),
+  h = document.getElementById("h"),
+  m = document.getElementById("m"),
+  l = document.getElementById("l");
 
 function calculate(val, base, which) {
-if (val.length === 0) {
-    d.value = null,
-    b.value = null,
-    o.value = null,
-    h.value = null
-    m.value = null
-    l.value = null
-    return
-} 
-let inputtedVal = parseFloat(val, base) // decimal value
+  if (val.length === 0) {
+    d.value = null;
+    b.value = null;
+    o.value = null;
+    h.value = null;
+    m.value = null;
+    l.value = null;
+    return;
+  }
 
-// results
-switch(which) {
-    case 'b':
-        d.value = inputtedVal.toString(10).toUpperCase()
-        o.value = inputtedVal.toString(8).toUpperCase()
-        h.value = inputtedVal.toString(16).toUpperCase()
-        m.value = Math.floor((b.value / Math.pow(10, b.value.toString().length - 1 )) % 10)
-        l.value = b.value % 10
-    break
+  let inputtedVal = val.includes("-")
+    ? -parseInt(val, base)
+    : parseInt(val, base); // handle negative numbers
 
-    case 'o':
-        d.value = inputtedVal.toString(10).toUpperCase()
-        b.value = inputtedVal.toString(2).toUpperCase()
-        h.value = inputtedVal.toString(16).toUpperCase()
-        m.value = Math.floor((b.value / Math.pow(10, b.value.toString().length - 1 )) % 10)
-        l.value = b.value % 10
-    break
+  // results
+  switch (which) {
+    case "b":
+      d.value = inputtedVal.toString(10).toUpperCase();
+      o.value = inputtedVal.toString(8).toUpperCase();
+      h.value = inputtedVal.toString(16).toUpperCase();
+      m.value = Math.floor(
+        (inputtedVal / Math.pow(10, inputtedVal.toString().length - 1)) % 10
+      );
+      l.value = inputtedVal % 10;
+      break;
 
-    case 'h':
-        d.value = inputtedVal.toString(10).toUpperCase()
-        b.value = inputtedVal.toString(2).toUpperCase()
-        o.value = inputtedVal.toString(8).toUpperCase()
-        m.value = inputtedVal.toString(8).toUpperCase()
-        l.value = inputtedVal.toString(8).toUpperCase()
-    break
+    case "o":
+      d.value = inputtedVal.toString(10).toUpperCase();
+      b.value = inputtedVal.toString(2).toUpperCase();
+      h.value = inputtedVal.toString(16).toUpperCase();
+      m.value = Math.floor(
+        (inputtedVal / Math.pow(10, inputtedVal.toString().length - 1)) % 10
+      );
+      l.value = inputtedVal % 10;
+      break;
+
+    case "h":
+      d.value = inputtedVal.toString(10).toUpperCase();
+      b.value = inputtedVal.toString(2).toUpperCase();
+      o.value = inputtedVal.toString(8).toUpperCase();
+      m.value = Math.floor(
+        (inputtedVal / Math.pow(10, inputtedVal.toString().length - 1)) % 10
+      );
+      l.value = inputtedVal % 10;
+      break;
 
     default:
-        b.value = inputtedVal.toString(2).toUpperCase()
-        o.value = inputtedVal.toString(8).toUpperCase()
-        h.value = inputtedVal.toString(16).toUpperCase()
-        m.value = Math.floor((b.value / Math.pow(10, b.value.toString().length - 1 )) % 10)
-        l.value = b.value % 10
+      b.value = inputtedVal.toString(2).toUpperCase();
+      o.value = inputtedVal.toString(8).toUpperCase();
+      h.value = inputtedVal.toString(16).toUpperCase();
+      m.value = Math.floor(
+        (inputtedVal / Math.pow(10, inputtedVal.toString().length - 1)) % 10
+      );
+      l.value = inputtedVal % 10;
+  }
 }
-}
+
